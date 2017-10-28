af3339b9f3f6c3643f9a4f28663120a3587e3add
YuJheLi <r06942074@ntu.edu.tw>
2017-10-28 17:44:58 +0800

    rm Download

--- hw1/Download.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlretrieve
-import sys
-
-file = 'RNN_best.model'
-url = 'https://doc-0o-7k-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1srbdtgfmfa07icvs2179sqls25btdg7/1509019200000/14093564571129775173/*/0B8xcn16IzjOeOWtLU2FIUmZYV3M?e=download'
-urlretrieve(url, file)
